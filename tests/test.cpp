9a7c2542dfce05e6db700065492b1b1f36af02b5
Lasar1k <alf.ivan2002@gmail.com>
2021-12-26 04:29:07 -0800

    update test

---
+++ tests/test.cpp
@@ -0,0 +1,11 @@
+// Copyright 2021 Lasar1k alf.ivan2002@gmail.com
+
+#include <stdexcept>
+
+#include <gtest/gtest.h>
+
+#include <example.hpp>
+
+TEST(Example, EmptyTest) {
+    EXPECT_THROW(example(), std::runtime_error);
+}
